21d204765bc91f8443d642545bb259ec1537f007
Orji Emmanuel <orjiemmanueleni@gmail.com>
2024-03-22 23:37:12 +0100

    recent update

--- redux/features/auth/authApi.ts
+++ redux/features/auth/authApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { userLoggedIn, userLoggedOut, userRegistration } from "./authSlice";
+import { userLoggedIn, userRegistration } from "./authSlice";
 
 type RegistrationResponse = {
   message: string;
