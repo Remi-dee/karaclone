e3de0420aafa1ceec47785dc7d993f8fa97e5964
Fakiya Favour <fakiyaoluwatosin@gmail.com>
2024-04-26 16:47:49 +0100

    fix spacing of nobarl layout

--- app/(authNOBars)/layout.tsx
+++ app/(authNOBars)/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <div className=" relative   bottom-0  w-full  items-center  justify-center  flex top-0 h-full  flex-col  ">
         <div>{children}</div>
 
-        <div className=" absolute  text-[#475467]  text-sm flex  right-[12rem] bottom-[1rem]   gap-x-[1rem] justify-center wf ">
+        <div className=" absolute   text-[#475467]  text-sm flex  right-[12rem] bottom-[1rem]   gap-x-[100px] justify-center wf ">
           <p>© FXkarasell 2024</p>
           <p className="  flex  items-center gap-x-[0.3rem]">
             <span className=" text-[0.4rem] text-[#4d525a]">⚫</span> Help
